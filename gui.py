f0e8df41935957d06c4cc4567d88f292bdd5e9bf
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2023-11-15 09:47:58 +0100

    moved config to config.toml, first settings included in settings window, state of video stream included in main gui

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from PIL import Image, ImageTk
 import threading
 import queue
 from tkinter import ttk
+import toml
 RES_OPTIONS = {
     "640x480": (640, 480),
     "1920x1080": (1920, 1080),
@@ -31,13 +32,14 @@ def list_available_cameras():
     return cameras
 
 class CameraApp:
-    def __init__(self, root, camera_index):
+    def __init__(self, root, camera_index, camera_list):
         self.root = root
         self.root.title("Camera GUI")
 
         self.video_label = tk.Label(root)
         self.video_label.pack()
 
+        self.available_cameras=camera_list
         self.initial_camera_index = camera_index  # Store initial camera index
         self.cap = cv2.VideoCapture(camera_index)
         self.q = queue.Queue()
@@ -49,18 +51,80 @@ class CameraApp:
 
         self.settings_button = tk.Button(root, text="Open Settings", command=self.open_settings)
         self.settings_button.pack()
+        self.resolutions = self.load_resolutions()
+        self.stream_status_label = tk.Label(root, text="Stream Status: Stopped", fg="red")
+        self.stream_status_label.pack()
 
+        self.frame_width_label = tk.Label(root, text="Frame Width: ")
+        self.frame_width_label.pack()
+
+        self.frame_height_label = tk.Label(root, text="Frame Height: ")
+        self.frame_height_label.pack()
+
+        self.frame_rate_label = tk.Label(root, text="Frame Rate: ")
+        self.frame_rate_label.pack()
         self.root.protocol("WM_DELETE_WINDOW", self.close_app)
+
+    def load_settings(self):
+        try:
+            with open('ccd_config.toml', 'r') as config_file:
+                config = toml.load(config_file)
+                self.initial_camera_index = config.get('initial_camera_index')
+                self.selected_resolution.set(config.get('initial_resolution'))
+        except FileNotFoundError:
+            print('Error')
+            self.initial_camera_index = 0
+            self.selected_resolution.set("640x480")
+        self.update_settings()
+    def load_resolutions(self):
+        resolutions = {}
+        try:
+            with open('ccd_config.toml', 'r') as config_file:
+                config = toml.load(config_file)
+                resolutions = config.get('resolutions', {})
+        except FileNotFoundError:
+            print('Error loading resolutions')
+
+        return resolutions
+    def save_settings(self):
+        config = {
+            "initial_camera_index": self.initial_camera_index,
+            "initial_resolution": self.selected_resolution.get()
+        }
+        with open('ccd_config.toml', 'w') as config_file:
+            toml.dump(config, config_file)
 
     def video_capture_thread(self):
         while not self.stop_event.is_set():
             ret, frame = self.cap.read()
-            if not ret:
+            if ret:
+                frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                frame_rate = self.cap.get(cv2.CAP_PROP_FPS)
+
+                self.frame_width_label.config(text=f"Frame Width: {frame_width}")
+                self.frame_height_label.config(text=f"Frame Height: {frame_height}")
+                self.frame_rate_label.config(text=f"Frame Rate: {frame_rate:.2f}")
+                self.stream_status_label.config(text="Stream Status: Running", fg="green")
+            else:
                 break
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(frame)
-            resized_frame = img.resize((640, 480), Image.ANTIALIAS)
+
+            # Get the actual camera stream resolution
+            actual_width = self.cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+            actual_height = self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+            # Calculate the ratio to maintain a minimum height of 480 pixels
+            ratio = float(actual_width/ actual_height)
+            new_width = int(480* ratio)
+            new_height = int(480)
+
+            # Resize the frame while maintaining the aspect ratio
+            resized_frame = img.resize((new_width, new_height), Image.ANTIALIAS)
             img = ImageTk.PhotoImage(resized_frame)
+
             try:
                 self.q.put(img, block=False)
             except queue.Full:
@@ -92,15 +156,32 @@ class CameraApp:
         settings_window.title("Settings")
         self.selected_resolution = tk.StringVar()
         self.selected_resolution.set("640x480")
+
         tk.Label(settings_window, text="Select Resolution:").pack()
-
         resolution_combobox = ttk.Combobox(settings_window, textvariable=self.selected_resolution,
-                                           values=list(RES_OPTIONS.keys()), state="readonly")
+                                           values=list(self.resolutions.keys()), state="readonly")
         resolution_combobox.pack()
+        tk.Label(settings_window, text="Select Camera:").pack()
+        camera_combobox = ttk.Combobox(settings_window, values=self.available_cameras, state="readonly")
+        camera_combobox.current(self.initial_camera_index)
+        camera_combobox.pack()
 
-        update_button = tk.Button(settings_window, text="Update", command=self.update_settings)
+        update_button = tk.Button(settings_window, text="Update camera", command=self.update_settings)
         update_button.pack()
 
+        save_button = tk.Button(settings_window, text="Save Settings to config", command=self.save_settings)
+        save_button.pack()
+
+        load_standard_button = tk.Button(settings_window, text="Load Settings from config", command=self.load_settings)
+        load_standard_button.pack()
+
+        camera_combobox.bind("<<ComboboxSelected>>", lambda event, cb=camera_combobox: self.change_camera(cb))
+
+    def change_camera(self, camera_combobox):
+        new_camera_index = camera_combobox.current()
+        if new_camera_index != self.initial_camera_index:
+            self.initial_camera_index = new_camera_index
+            self.update_settings()  # Apply the new camera settings
     def update_settings(self):
         selected_resolution = self.selected_resolution.get()
         selected_width, selected_height = RES_OPTIONS[selected_resolution]
@@ -110,6 +191,19 @@ class CameraApp:
             if self.cap.isOpened():
                 self.cap.release()
             self.cap = cv2.VideoCapture(self.initial_camera_index)  # Use initial camera index
+            ret, frame = self.cap.read()
+            if ret:
+                frame_width = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                frame_height = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                frame_rate = self.cap.get(cv2.CAP_PROP_FPS)
+
+                self.frame_width_label.config(text=f"Frame Width: {frame_width}")
+                self.frame_height_label.config(text=f"Frame Height: {frame_height}")
+                self.frame_rate_label.config(text=f"Frame Rate: {frame_rate:.2f}")
+                self.stream_status_label.config(text="Stream Status: Running", fg="green")
+            else:
+                # If the stream is stopped
+                self.stream_status_label.config(text="Stream Status: Stopped", fg="red")
             self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, selected_width)
             self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, selected_height)
 
@@ -133,7 +227,7 @@ def main():
 
         if camera_index < len(cameras):
             root = tk.Tk()
-            app = CameraApp(root, camera_index)
+            app = CameraApp(root, camera_index, cameras)
             app.update_video()  # Start the video update loop
             root.mainloop()
         else:
