06b47667d7d05a1593020e23e90dd3bc79011012
Julian-Hochhaus <julian.hochhaus@tu-dortmund.de>
2023-11-28 10:51:34 +0100

    bump version 0.2.1 -> 0.3.0

--- gui.py
+++ gui.py
@@ -16,7 +16,7 @@ import tkinter.filedialog as filedialog
 
 script_directory = os.path.dirname(os.path.abspath(__file__))
 settings_window = None
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 def list_available_cameras():
